dea705a6b612d2064dc1f4fc59a6bd9bdf6a7346
mariusz89016 <mariusz89016@gmail.com>
2016-01-11 12:14:51 +0100

    Added DB test (cleanDatabase should not remove tables from database

--- src/test/scala/pl/edu/agh/iet/akka_tracing/sbt/DatabaseTasksSpec.scala
+++ src/test/scala/pl/edu/agh/iet/akka_tracing/sbt/DatabaseTasksSpec.scala
@@ -2,12 +2,12 @@ package pl.edu.agh.iet.akka_tracing.sbt
 
 import java.io.{FileWriter, BufferedWriter, File}
 import java.util.UUID
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfterEach, FlatSpec}
 import slick.jdbc.meta.MTable
 import scala.concurrent.duration._
 import scala.concurrent.Await
 
-class DatabaseTasksSpec extends FlatSpec {
+class DatabaseTasksSpec extends FlatSpec with BeforeAndAfterEach {
   val configFile = File.createTempFile("akka_tracing.conf.test.in-memory-db", "")
   val bufferedWriter = new BufferedWriter(new FileWriter(configFile))
   bufferedWriter.write(
@@ -43,16 +43,23 @@ class DatabaseTasksSpec extends FlatSpec {
     assert(afterInitTables.map(_.name.name) === List("messages", "relation"))
   }
 
-  it should """remove rows from tables "messages" & "relation"""" in {
+  it should """do nothing when "initDatabase" method called more than once""" in {
     databaseTasks.initDatabase()
-    val randomUUID = UUID.randomUUID()
-    Await.result(db.run(DBIO.seq(
-      sqlu"""insert into "messages" values(${randomUUID.toString}, 'sender', 'receiver')""",
-      sqlu"""insert into "relation" values(${randomUUID.toString}, ${randomUUID.toString})"""
-    )), 1 seconds)
+    insertData
 
-    val countMessagesQuery = sql"""select count(*) from "messages"""".as[Int]
-    val countRelationQuery = sql"""select count(*) from "relation"""".as[Int]
+    databaseTasks.initDatabase()
+
+    val afterMessagesRowsCount = Await.result(db.run(countMessagesQuery), 1 seconds).head
+    val afterRelationRowsCount = Await.result(db.run(countRelationQuery), 1 seconds).head
+
+    assert(afterMessagesRowsCount === 1)
+    assert(afterRelationRowsCount === 1)
+  }
+
+  """A DatabaseTasks "cleanDatabase"""" should """remove rows from tables "messages" & "relation"""" in {
+    databaseTasks.initDatabase()
+    insertData
+
     val beforeMessagesRowsCount = Await.result(db.run(countMessagesQuery), 1 seconds).head
     val beforeRelationRowsCount = Await.result(db.run(countRelationQuery), 1 seconds).head
 
@@ -68,22 +75,30 @@ class DatabaseTasksSpec extends FlatSpec {
     assert(afterRelationRowsCount === 0)
   }
 
-  it should """do nothing when "initDatabase" method called more than once""" in {
+  it should "not remove tables from database" in {
     databaseTasks.initDatabase()
+    databaseTasks.cleanDatabase()
+    val tables = Await.result(db.run(MTable.getTables), 1 seconds).toList
+
+    assert(tables.size === 2)
+    assert(tables.map(_.name.name) === List("messages", "relation"))
+  }
+
+  override protected def afterEach(): Unit = {
+    Await.result(db.run(DBIO.seq(
+      sqlu"""drop table if exists "messages"""",
+      sqlu"""drop table if exists "relation""""
+    )), 1 seconds)
+  }
+
+  def insertData: Unit = {
     val randomUUID = UUID.randomUUID()
     Await.result(db.run(DBIO.seq(
       sqlu"""insert into "messages" values(${randomUUID.toString}, 'sender', 'receiver')""",
       sqlu"""insert into "relation" values(${randomUUID.toString}, ${randomUUID.toString})"""
     )), 1 seconds)
-
-    databaseTasks.initDatabase()
-
-    val countMessagesQuery = sql"""select count(*) from "messages"""".as[Int]
-    val countRelationQuery = sql"""select count(*) from "relation"""".as[Int]
-    val afterMessagesRowsCount = Await.result(db.run(countMessagesQuery), 1 seconds).head
-    val afterRelationRowsCount = Await.result(db.run(countRelationQuery), 1 seconds).head
-
-    assert(afterMessagesRowsCount === 1)
-    assert(afterRelationRowsCount === 1)
   }
+
+  val countMessagesQuery = sql"""select count(*) from "messages"""".as[Int]
+  val countRelationQuery = sql"""select count(*) from "relation"""".as[Int]
 }
