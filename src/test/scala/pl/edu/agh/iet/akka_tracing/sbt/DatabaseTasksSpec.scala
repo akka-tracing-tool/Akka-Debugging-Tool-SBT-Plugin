2583fea81c42dc991618fd3394320733dfd33832
mariusz89016 <mariusz89016@gmail.com>
2016-01-11 10:59:42 +0100

    Refactored DatabaseTasksSpec

--- src/test/scala/pl/edu/agh/iet/akka_tracing/sbt/DatabaseTasksSpec.scala
+++ src/test/scala/pl/edu/agh/iet/akka_tracing/sbt/DatabaseTasksSpec.scala
@@ -2,11 +2,9 @@ package pl.edu.agh.iet.akka_tracing.sbt
 
 import java.io.{FileWriter, BufferedWriter, File}
 import java.util.UUID
-
 import org.scalatest.FlatSpec
 import slick.jdbc.meta.MTable
 import scala.concurrent.duration._
-
 import scala.concurrent.Await
 
 class DatabaseTasksSpec extends FlatSpec {
@@ -33,35 +31,40 @@ class DatabaseTasksSpec extends FlatSpec {
   import slick.driver.H2Driver.api._
   val db = Database.forURL("jdbc:h2:mem:test")
 
-  "A DatabaseTasks \"initDatabase\"" should "create empty tables" in {
+  """A DatabaseTasks "initDatabase"""" should "create empty tables" in {
     val beforeInitTables = Await.result(db.run(MTable.getTables), 1 seconds).toList
+
     assert(beforeInitTables.size === 0)
 
     databaseTasks.initDatabase()
     val afterInitTables = Await.result(db.run(MTable.getTables), 1 seconds).toList
+
     assert(afterInitTables.size === 2)
     assert(afterInitTables.map(_.name.name) === List("messages", "relation"))
   }
 
-  it should "remove rows from tables \"messages\" & \"relation\"" in {
-    val randomUUID = UUID.randomUUID()
+  it should """remove rows from tables "messages" & "relation"""" in {
     databaseTasks.initDatabase()
+    val randomUUID = UUID.randomUUID()
     Await.result(db.run(DBIO.seq(
       sqlu"""insert into "messages" values(${randomUUID.toString}, 'sender', 'receiver')""",
       sqlu"""insert into "relation" values(${randomUUID.toString}, ${randomUUID.toString})"""
     )), 1 seconds)
 
-    val resultMessages = Await.result(db.run(sql"""select count(*) from "messages"""".as[Int]), 1 seconds).head
-    val resultRelation = Await.result(db.run(sql"""select count(*) from "relation"""".as[Int]), 1 seconds).head
-    assert(resultMessages === 1)
-    assert(resultRelation === 1)
+    val countMessagesQuery = sql"""select count(*) from "messages"""".as[Int]
+    val countRelationQuery = sql"""select count(*) from "relation"""".as[Int]
+    val beforeMessagesRowsCount = Await.result(db.run(countMessagesQuery), 1 seconds).head
+    val beforeRelationRowsCount = Await.result(db.run(countRelationQuery), 1 seconds).head
+
+    assert(beforeMessagesRowsCount === 1)
+    assert(beforeRelationRowsCount === 1)
 
     databaseTasks.cleanDatabase()
-    val afterResultMessages = Await.result(db.run(sql"""select count(*) from "messages"""".as[Int]), 1 seconds).head
-    val afterResultRelation = Await.result(db.run(sql"""select count(*) from "relation"""".as[Int]), 1 seconds).head
-    assert(afterResultMessages === 0)
-    assert(afterResultRelation === 0)
-//    val afterInitTables = Await.result(db.run(MTable.getTables), 1 seconds).toList
-  }
 
+    val afterMessagesRowsCount = Await.result(db.run(countMessagesQuery), 1 seconds).head
+    val afterRelationRowsCount = Await.result(db.run(countRelationQuery), 1 seconds).head
+
+    assert(afterMessagesRowsCount === 0)
+    assert(afterRelationRowsCount === 0)
+  }
 }
